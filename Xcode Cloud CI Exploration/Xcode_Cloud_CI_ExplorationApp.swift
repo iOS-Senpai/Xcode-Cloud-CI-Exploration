56d4b52c82dd3f782c13c1495f04d687ee9196ff
iOS-Senpai <pradeep.iossenpai@gmail.com>
2024-04-30 15:07:14 +0530

    Initial Commit

---
+++ Xcode Cloud CI Exploration/Xcode_Cloud_CI_ExplorationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_Cloud_CI_ExplorationApp.swift
+//  Xcode Cloud CI Exploration
+//
+//  Created by xavient on 30/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Xcode_Cloud_CI_ExplorationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
